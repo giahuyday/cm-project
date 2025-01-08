d72945b6e296da0d50e4388cbd01c8e6a93e017c
giahuyday <huyhoangnguyen301202@gmail.com>
2025-01-08 22:00:42 +0700

    feat: Update CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,18 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter/exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { GraphQLErrorFilter } from './common/filter/graphql.exception.filter';
+import { ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: configService.get<string>('ALLOW_ORIGIN').split(','),
+        methods: configService.get<string>('ALLOW_METHOD').split(','),
+        allowedHeaders: configService.get<string>('ALLOW_HEADER').split(','),
+    });
+
     app.useGlobalFilters(new HttpExceptionFilter(), new GraphQLErrorFilter());
     app.useGlobalPipes(
         new ValidationPipe({
